9f0d114d3d934175e34ff19aca2fac85afc58bd2
Arthur <arthurd800@wp.pl>
2020-07-23 10:30:51 +0200

    setting up first projects

--- vue_front.js
+++ vue_front.js
@@ -1,18 +1,3 @@
 new Vue({
     el: '#exercise',
-    data: {
-        show: false,
-        array: ['Max', 'Anna', 'Chris', 'Manu'],
-        cars: ['toyota', 'bmw', 'hyundai', 'ford', 'skoda', 'audi', 'suzuki'],
-        myObject: {
-            title: 'Lord of the Rings',
-            author: 'J.R.R. Tolkiens',
-            books: '3',
-        },
-        testData: {
-            name: 'TESTOBJECT',
-            id: 10,
-            data: [1.67, 1.33, 0.98, 2.21],
-        },
-    },
 });
